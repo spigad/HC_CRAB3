c25d2ef18617d321dc737a55687faf142efebcc0
Mario Úbeda García <mario.ubeda.garcia@cern.ch>
2011-05-03 11:12:53 +0000

    remove testdir from svn

--- hammercloud/apps/atlas/testdirs/run-test-8.sh
+++
@@ -1,8 +0,0 @@
-cd /data/hc/apps/atlas
-mkdir -p testdirs/test_8
-if [ -e "testdirs/test_8/stdouterr.txt" ]
-then
-  mv testdirs/test_8/stdouterr.txt testdirs/test_8/stdouterr.txt.`date +%s`
-fi
-cd /data/hc
-./scripts/submit/test-run.sh atlas /data/hc/external/ganga/install/HEAD/bin/ganga 8 -m default &> /data/hc/apps/atlas/testdirs/test_8/stdouterr.txt
