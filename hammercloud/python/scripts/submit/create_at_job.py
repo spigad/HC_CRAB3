1f6a3b995b06295ed0d9f00c2e9efaba9947d206
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2011-07-29 14:05:48 +0000

    Improved the create_at_job script to recover running tests.

--- hammercloud/python/scripts/submit/create_at_job.py
+++ hammercloud/python/scripts/submit/create_at_job.py
@@ -3,7 +3,21 @@ from django.db.models import Min
 
 import commands,time,os,os.path
 
+me = 'hammercloud-noreply@cern.ch'
+#you = 'daniel.colin.vanderster@cern.ch,0041764870482@sms.switch.ch'                                                                                                                  
+you = 'daniel.colin.vanderster@cern.ch'
+
 class CreateAtJob:
+
+  def send_alert(self, msg):
+    sendmail_location = "/usr/sbin/sendmail" # sendmail location                                                                                                                      
+    p = os.popen("%s -t" % sendmail_location, "w")
+    p.write("From: %s\n" % me)
+    p.write("To: %s\n" % you)
+    p.write("Subject: HammerCloud Test Restarted\n")
+    p.write("\n") # blank line separating headers from body                                                                                                                           
+    p.write(msg)
+    status = p.close()
 
   def getHCAPP(self,app):
     HCAPP = os.getenv('HCAPP')
@@ -20,6 +34,62 @@ class CreateAtJob:
       return 0
     else:
       return HCDIR
+  
+  def logFileUpdated(self,app,test):
+    try:
+      mtime = os.stat('%s/testdirs/test_%d/stdouterr.txt'%(HCAPP,test.id)).st_mtime
+    except OSError:
+      print '[ERROR][%s][create_at_job] stdouterr.txt for test %d doesn\'t exist!'%(app,test.id)
+      return False
+    age = int(time.time())-mtime
+    if age > 60:
+      print '[ERROR][%s][create_at_job] stdouterr.txt for test %d not updated for %d s'%(app,test.id,age)
+      return False
+    return True
+
+  def gangaProcessRunning(self,app,test):
+    cmd = 'ps -ef | grep bin/ganga | grep -v grep | grep %d'%test.id
+    if os.system(cmd):
+      print '[ERROR][%s][create_at_job] ganga process does not appear to be running for test %d'%(app,test.id)
+      return False
+    return True
+
+  def testScriptRunning(self,app,test):
+    # check if process is running                                                                                                                                                     
+    cmd = 'ps -ef | grep "./hc-test-run.sh %d" | grep -v grep'%test.id
+    if os.system(cmd):
+      print '[ERROR][%s][create_at_job] test script does not appear to be running for test %d'%(app,test.id)
+      return False
+    return True
+
+  def testPaused(self,app,test):
+    txt = 'Test %d is paused'%test.id
+    cmd = 'grep "%s" %s/testdirs/test_%d/stdouterr.txt'%(txt,HCAPP,test.id)
+    if os.system(cmd):
+        print '[ERROR][%s][create_at_job] test %d is not paused'%(app,test.id)
+        return False
+    return True
+
+  def checkFault(self,app,test):
+    # check logfile for JobAccessError or segmentation fault                                                                                                                          
+    txt = ['JobAccessError','Segmentation fault']
+    for t in txt:
+      cmd = 'grep "%s" %s/testdirs/test_%d/stdouterr.txt'%(t,HCAPP,test.id)
+      if not os.system(cmd):
+        print '[ERROR][%s][create_at_job] %s found in stdouterr.txt for test %d'%(app,t,test.id)
+        return True
+    return False
+
+  def stateFileExists(self,app,test):
+    # check for state file in /tmp                                                                                                                                                    
+    try:
+      mtime = os.stat('/tmp/hc-cron.running.%d'%test.id).st_mtime
+      os.remove('/tmp/hc-cron.running.%d'%test.id)
+    except OSError:
+      print '[ERROR][%s][create_at_job] hc-cron.running.%d doesn\'t exist'%(app,test.id)
+      return False
+    return True
+
 
   def createScript(self,app,test):
 
@@ -60,6 +130,14 @@ class CreateAtJob:
     print '[INFO][%s][create_at_job] Starting test %s: %s\n'%(app,test.id,atjobid)
     return 1
 
+  def rescheduleJob(self,app,test):
+    atjobid = commands.getoutput('at -f %s/testdirs/run-test-%d.sh now'%(app,test.id)).rstrip()
+    test.atjobid = int(atjobid.split()[1])
+    test.save()
+    print '[INFO][%s][create_at_job] restarting test %d: %s\n'%(app,test.id,atjobid)
+    return atjobid
+
+
   def run(self,app,dic):
 
     if app == 'core':
@@ -93,6 +171,33 @@ class CreateAtJob:
           print '[INFO][%s][create_at_job] Test %s NOT assigned to %s.'%(app,test.id,hostname) 
           print '[INFO][create_at_job][%s] Registered loads.'%(app)
           for thl in test_hosts:
-            print '%s: %3f'%(thl.host.name,thl.host.loadavg1m)	        
+            print '%s: %3f'%(thl.host.name,thl.host.loadavg1m)
+    
+    test = test.objects.fitler(state='running')
+    if not tests:
+      print '[INFO][%s][create_at_job] No tests found on state: running'%(app)
+    
+    for test in tests:
+      if self.logFileUpdated(app, test):
+        continue
+
+      if self.gangaProcessRunning(app, test):
+        continue
+
+      if self.testScriptRunning(app, test):
+        continue
+
+      if self.testPaused(app, test):
+        continue
+
+      self.checkFault(app, test)
+      
+      if self.stateFileExists(app, test):
+        print '[INFO][%s][create_at_job] Test %d on %s had lockfile removed.'%(app,test.id,hostname) 
+        self.send_alert('%s\nTest %d on %s had lockfile removed.'%(time.ctime(),test.id,hostname))
+        
+      res = self.rescheduleJob(app, test)
+      print '[INFO][%s][create_at_job] Test %d on %s has been restarted: %s.'%(app,test.id,hostname,res) 
+      self.send_alert('%s\nTest %d on %s has been restarted: %s.'%(time.ctime(),test.id,hostname,res))
 
     return 1
