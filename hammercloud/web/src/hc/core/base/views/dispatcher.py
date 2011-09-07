5466d5a1ce1f0d10be80271a879d055547103709
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2011-09-07 13:42:06 +0000

    Protect the dispatcher for not found URIs.

--- hammercloud/web/src/hc/core/base/views/dispatcher.py
+++ hammercloud/web/src/hc/core/base/views/dispatcher.py
@@ -2,12 +2,13 @@ from django.http import Http404
 from hc.core.utils.generic.class_func import custom_import
 
 def dispatcher(request,app,*args,**kwargs):
-  
+  res = None
   func = kwargs.pop('func')
  
   try:
     m = custom_import('hc.'+app+'.views.'+func)
+    res = m(request,*args,**kwargs)
   except:
     raise Http404
 
-  return m(request,*args,**kwargs)
+  return res
