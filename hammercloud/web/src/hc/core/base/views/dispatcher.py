0e653de19ad721ebe8e28019fd76537120ea7e9f
Ganga Robot <gangar.bt@cern.ch>
2011-11-07 09:14:39 +0000

    Solve the error notifications on a malformed URL call.

--- hammercloud/web/src/hc/core/base/views/dispatcher.py
+++ hammercloud/web/src/hc/core/base/views/dispatcher.py
@@ -4,11 +4,12 @@ from hc.core.utils.generic.class_func import custom_import
 def dispatcher(request,app,*args,**kwargs):
   res = None
   func = kwargs.pop('func')
- 
+
   try:
     m = custom_import('hc.'+app+'.views.'+func)
-    res = m(request,*args,**kwargs)
-  except:
-    raise
+    if m is None:
+      raise TypeError()
+    return m(request,*args,**kwargs)
+  except TypeError:
+    raise Http404
 
-  return res
