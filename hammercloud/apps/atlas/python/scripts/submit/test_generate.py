c500d4181f415ec3d73c8b7ede5c4005353f6f7f
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2011-12-20 16:20:05 +0000

    Added the T3 PFN support properly.

--- hammercloud/apps/atlas/python/scripts/submit/test_generate.py
+++ hammercloud/apps/atlas/python/scripts/submit/test_generate.py
@@ -90,35 +90,33 @@ class TestGenerate:
 
     # Dataset patterns
     datasetpatterns = []
-    pfnlistfile = None
     test_dspatterns = test.getTestDspatterns_for_test.all()
-    if mode == 't3':
-      pfnlistfile = test_dspatterns[0].dspattern.pattern
-    else:
-      for td in test_dspatterns:
-        if td.dspattern.pattern.startswith('/'):
-          file = open(td.dspattern.pattern)
-          for l in file:
-            datasetpatterns.append(l.strip())
-          file.close()
-        elif td.dspattern.pattern.startswith('http'):
-          import urllib2
-          url = td.dspattern.pattern
-          try:
-            patterns = urllib2.urlopen(url).read().split()
-            print "Downloaded dspatterns", url, patterns
-            for p in patterns:
-              if p not in datasetpatterns:
-                datasetpatterns.append(p)
-          except:
-            print 'failed to download url pattern',url
-        elif td.dspattern.pattern.endswith('.txt'):
-          file = open(basePath + '/inputfiles/templates/' + td.dspattern.pattern)
-          for l in file:
-            datasetpatterns.append(l.strip())
-          file.close()
-        else:
-          datasetpatterns.append(td.dspattern.pattern)
+    for td in test_dspatterns:
+      if mode == 't3':
+        datasetpatterns.append(td.dspattern.pattern)
+      elif td.dspattern.pattern.startswith('/'):
+        file = open(td.dspattern.pattern)
+        for l in file:
+          datasetpatterns.append(l.strip())
+        file.close()
+      elif td.dspattern.pattern.startswith('http'):
+        import urllib2
+        url = td.dspattern.pattern
+        try:
+          patterns = urllib2.urlopen(url).read().split()
+          print "Downloaded dspatterns", url, patterns
+          for p in patterns:
+            if p not in datasetpatterns:
+              datasetpatterns.append(p)
+        except:
+          print 'failed to download url pattern',url
+      elif td.dspattern.pattern.endswith('.txt'):
+        file = open(basePath + '/inputfiles/templates/' + td.dspattern.pattern)
+        for l in file:
+          datasetpatterns.append(l.strip())
+        file.close()
+      else:
+        datasetpatterns.append(td.dspattern.pattern)
 
     if len(datasetpatterns) == 0:
       print 'No dspatterns found for this test!'
@@ -159,13 +157,16 @@ class TestGenerate:
       print "Datasets by pattern available at",location
       datasets = []
       for datasetpattern in datasetpatterns:
-        print time.ctime()
-        print 'dq2.listDatasetsByNameInSite(site=%s, name=%s)'%(repr(location),repr(datasetpattern))
-        temp = dq2.listDatasetsByNameInSite(site=location, name=datasetpattern)
-        temp = list(temp)
-        datasets = datasets + temp
-        print datasetpattern, location, len(temp)
-        print time.ctime()
+        if mode == 't3':
+          print 'USING LFN %s' % datasetpattern
+        else:
+          print time.ctime()
+          print 'dq2.listDatasetsByNameInSite(site=%s, name=%s)'%(repr(location),repr(datasetpattern))
+          temp = dq2.listDatasetsByNameInSite(site=location, name=datasetpattern)
+          temp = list(temp)
+          datasets = datasets + temp
+          print datasetpattern, location, len(temp)
+          print time.ctime()
       datasetList[location] = datasets
       print
 
@@ -267,34 +268,35 @@ class TestGenerate:
                 print 'Skipping %s' %dataset
                 continue
 
-              # Get dataset info
-              try:
-                print time.ctime()
-                print 'dq2.listFileReplicas(%s, %s)'%(repr(location),repr(dataset))
-                datasetsiteinfo = dq2.listFileReplicas(location, dataset)
-                print time.ctime()
-              except:
-                print 'crash: %s, %s' %(location, dataset)
-                continue
-              # Skip is data is not immutable
-              try:
-                immutable = datasetsiteinfo[0]['immutable']
-              except:
-                continue
-              if not immutable:
-                print 'Not immutable, skip %s, ' %dataset
-                continue
-              # Skip dataset if not complete at site
-              try:
-                print time.ctime()
-                print 'dq2.listDatasetReplicas(%s)[dq2.listDatasets(%s)[%s][\'vuids\'][0]][0]'%(repr(dataset),repr(dataset),repr(dataset))
-                incompleteLocations = dq2.listDatasetReplicas(dataset)[dq2.listDatasets(dataset)[dataset]['vuids'][0]][0]
-                print time.ctime()
-              except:
-                incompleteLocations = []
-              if location in incompleteLocations:
-                print 'Not complete at %s, skip %s' %(location, dataset)
-                continue
+              if mode != 't3':
+                # Get dataset info
+                try:
+                  print time.ctime()
+                  print 'dq2.listFileReplicas(%s, %s)'%(repr(location),repr(dataset))
+                  datasetsiteinfo = dq2.listFileReplicas(location, dataset)
+                  print time.ctime()
+                except:
+                  print 'crash: %s, %s' %(location, dataset)
+                  continue
+                # Skip is data is not immutable
+                try:
+                  immutable = datasetsiteinfo[0]['immutable']
+                except:
+                  continue
+                if not immutable:
+                  print 'Not immutable, skip %s, ' %dataset
+                  continue
+                # Skip dataset if not complete at site
+                try:
+                  print time.ctime()
+                  print 'dq2.listDatasetReplicas(%s)[dq2.listDatasets(%s)[%s][\'vuids\'][0]][0]'%(repr(dataset),repr(dataset),repr(dataset))
+                  incompleteLocations = dq2.listDatasetReplicas(dataset)[dq2.listDatasets(dataset)[dataset]['vuids'][0]][0]
+                  print time.ctime()
+                except:
+                  incompleteLocations = []
+                if location in incompleteLocations:
+                  print 'Not complete at %s, skip %s' %(location, dataset)
+                  continue
 
 #              numberoffiles = numberoffiles + datasetsiteinfo[0]['found']
 #              if numberoffiles>0:
@@ -335,17 +337,12 @@ class TestGenerate:
                 print 'symlink already exists'
                 pass
 
-            if mode == 't3':
-              dataset_str = pfnlistfile
-            else:
-              dataset_str = repr(datasetAll)
-
             outFile_content = outFile_content.replace('####USERAREA####', tmp_userarea)
             outFile_content = outFile_content.replace('####JOBOPTIONS####', joboptions)
             outFile_content = outFile_content.replace('####JOBOPTIONSFILENAME####', joboptionsfilename)
             outFile_content = outFile_content.replace('####SITES####', repr(str(site)))
             outFile_content = outFile_content.replace('####OUTPUTDATASETNAME####', outputdatasetname+'.'+site+'.'+str(fid) )
-            outFile_content = outFile_content.replace('####DATASET####', dataset_str)
+            outFile_content = outFile_content.replace('####DATASET####', repr(datasetAll))
             outFile_content = outFile_content.replace('####INPUTTYPE####', inputtype)
             outFile_content = outFile_content.replace('####NUM####', repr((num-1)))
             outFile_content = outFile_content.replace('####TESTID####', str(test.id))
