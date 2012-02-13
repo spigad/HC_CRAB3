435b70777a2647e8c23d62d87388698fad78cb01
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-02-13 10:36:29 +0000

    Added the SLS status bar for all the instances.

--- hammercloud/web/src/hc/core/base/views/configuration.py
+++ hammercloud/web/src/hc/core/base/views/configuration.py
@@ -1,6 +1,9 @@
 PARTICULARITIES = {'atlas':{'cloud':'Cloud',
-                            'base_template': 'atlas/base.html'},
+                            'base_template': 'atlas/base.html',
+                            'sls': 'HC.ATLAS'},
                    'cms'  :{'cloud':'Region',
-                            'base_template': 'core/app/base.html'},
+                            'base_template': 'core/app/base.html',
+                            'sls': 'HC.CMS'},
                    'lhcb' :{'cloud':'T1',
-                            'base_template': 'core/app/base.html'}}
+                            'base_template': 'core/app/base.html',
+                            'sls': 'HC.LHCb'}}
