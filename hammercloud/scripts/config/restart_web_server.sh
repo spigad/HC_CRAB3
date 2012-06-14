c5a9bc39ff72057333cffcec380501560be0b384
Ganga Robot <gangar.bt@cern.ch>
2012-06-14 15:23:03 +0000

    Add memcached to the restart script.

--- hammercloud/scripts/config/restart_web_server.sh
+++ hammercloud/scripts/config/restart_web_server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 /etc/init.d/varnish stop
-/etc/init.d/httpd restart
+/etc/init.d/httpd stop
+/etc/init.d/memcached stop
+/etc/init.d/memcached start
+/etc/init.d/httpd start
 /etc/init.d/varnish start
