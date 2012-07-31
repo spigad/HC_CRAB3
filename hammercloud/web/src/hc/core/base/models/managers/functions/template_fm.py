723b59b85b1ec62ce4d866b3e9956163bfb703bc
Ganga Robot <gangar.bt@cern.ch>
2012-07-31 13:45:24 +0000

    Improved template function manager with select_related().

--- hammercloud/web/src/hc/core/base/models/managers/functions/template_fm.py
+++ hammercloud/web/src/hc/core/base/models/managers/functions/template_fm.py
@@ -1,16 +1,26 @@
-#from django.db import models
-
 def getTemplateBackendNames(template):
-  return [tb.backend.name for tb in template.getTemplateBackends_for_template.all()]
+  """Returns the list of backends names."""
+  return [tb.backend.name for tb in (template.getTemplateBackends_for_template
+                                             .select_related('backend')
+                                             .all())]
+
 
 def getTemplateSiteNames(template):
-  return [ts.site.name for ts in template.getTemplateSites_for_template.all()]
+  """Returns the list of site names."""
+  return [ts.site.name for ts in (template.getTemplateSites_for_template
+                                          .select_related('site')
+                                          .all())]
+
 
 def  getTemplateCloudCodes(template):
-  ################################################## WHY DISTINCT ??
-  return [tc.cloud.code for tc in template.getTemplateClouds_for_template.all()]
+  """Returns the list of cloud names."""
+  return [tc.cloud.code for tc in (template.getTemplateClouds_for_template
+                                           .select_related('cloud')
+                                           .all())]
+
 
 def getTemplateHostNames(template):
-  return [th.host.name for th in template.getTemplateHosts_for_template.all()]
-
-
+  """Returns the host names."""
+  return [th.host.name for th in (template.getTemplateHosts_for_template
+                                          .select_related('host')
+                                          .all())]
