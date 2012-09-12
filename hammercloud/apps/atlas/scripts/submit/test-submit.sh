51d1541296cc05d0ec792370afc2441c01d1c04e
Ganga Robot <gangar.bt@cern.ch>
2012-09-12 09:19:43 +0000

    Enable verbose mode for Ganga logging in ATLAS.

--- hammercloud/apps/atlas/scripts/submit/test-submit.sh
+++ hammercloud/apps/atlas/scripts/submit/test-submit.sh
@@ -26,9 +26,9 @@ then
     source /afs/cern.ch/sw/ganga/install/etc/setup-atlas.sh latest
 fi
 export GANGA_CONFIG_PATH=$GANGA_CONFIG_PATH:GangaAtlas/Atlas.ini:$HCAPP/config/GangaAtlas.ini.50
-
-echo $GANGABIN --config=$HCAPP/config/$APP-ROBOTStart.INI.50 -o[Configuration]gangadir=$HCAPP/testdirs/test_$1/gangadir -o[Logging]_logfile=$HCAPP/testdirs/test_$1/ganga.log -o[Panda]processingType=hammercloud -o[LCG]JobLogHandler=DQ2 -o[MonitoringServices]Athena/LCG=Ganga.Lib.MonitoringServices.Dashboard.LCGAthenaMS.LCGAthenaMS -o[DQ2]OUTPUTDATASET_LIFETIME='2_days' $2 robot run $HCAPP/testdirs/test_$1/jobs/*
-$GANGABIN --config=$HCAPP/config/$APP-ROBOTStart.INI.50 -o[Configuration]gangadir=$HCAPP/testdirs/test_$1/gangadir -o[Logging]_logfile=$HCAPP/testdirs/test_$1/ganga.log -o[Panda]processingType=hammercloud -o[LCG]JobLogHandler=DQ2 -o[MonitoringServices]Athena/LCG=Ganga.Lib.MonitoringServices.Dashboard.LCGAthenaMS.LCGAthenaMS -o[DQ2]OUTPUTDATASET_LIFETIME='2_days' $2 robot run $HCAPP/testdirs/test_$1/jobs/*
-
-
+ 
+echo $GANGABIN --config=$HCAPP/config/$APP-ROBOTStart.INI.50 -o[Logging]_format='VERBOSE' -o[Configuration]gangadir=$HCAPP/testdirs/test_$1/gangadir -o[Logging]_logfile=$HCAPP/testdirs/test_$1/ganga.log -o[Panda]processingType=hammercloud -o[LCG]JobLogHandler=DQ2 -o[MonitoringServices]Athena/LCG=Ganga.Lib.MonitoringServices.Dashboard.LCGAthenaMS.LCGAthenaMS -o[DQ2]OUTPUTDATASET_LIFETIME='2_days' $2 robot run $HCAPP/testdirs/test_$1/jobs/*
+$GANGABIN --config=$HCAPP/config/$APP-ROBOTStart.INI.50 -o[Logging]_format='VERBOSE' -o[Configuration]gangadir=$HCAPP/testdirs/test_$1/gangadir -o[Logging]_logfile=$HCAPP/testdirs/test_$1/ganga.log -o[Panda]processingType=hammercloud -o[LCG]JobLogHandler=DQ2 -o[MonitoringServices]Athena/LCG=Ganga.Lib.MonitoringServices.Dashboard.LCGAthenaMS.LCGAthenaMS -o[DQ2]OUTPUTDATASET_LIFETIME='2_days' $2 robot run $HCAPP/testdirs/test_$1/jobs/*
+#echo $GANGABIN --config=$HCAPP/config/$APP-ROBOTStart.INI.50 -o[Logging]_format='VERBOSE' -o[Configuration]gangadir=$HCAPP/testdirs/test_$1/gangadir -o[Logging]_logfile=$HCAPP/testdirs/test_$1/ganga.log -o[Panda]processingType=hammercloud -o[DQ2]OUTPUTDATASET_LIFETIME='2_days' $2 robot run $HCAPP/testdirs/test_$1/jobs/*
+#$GANGABIN --config=$HCAPP/config/$APP-ROBOTStart.INI.50 -o[Logging]_format='VERBOSE' -o[Configuration]gangadir=$HCAPP/testdirs/test_$1/gangadir -o[Logging]_logfile=$HCAPP/testdirs/test_$1/ganga.log -o[Panda]processingType=hammercloud -o[DQ2]OUTPUTDATASET_LIFETIME='2_days' $2 robot run $HCAPP/testdirs/test_$1/jobs/*
 
