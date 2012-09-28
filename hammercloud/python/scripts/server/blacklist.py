59ddaca05dc249d0c8c1de377b626b3aae8764b1
Ganga Robot <gangar.bt@cern.ch>
2012-09-28 10:09:43 +0000

    Add global autoexclusion disable in the autoexclusion script.

--- hammercloud/python/scripts/server/blacklist.py
+++ hammercloud/python/scripts/server/blacklist.py
@@ -8,6 +8,15 @@ class Blacklist:
       print '[ERROR][%s][blacklist] not available at core app.' % (app)
       return 1
 
+    # Check if the autoexclusion is enabled in the global options.
+    global_option = custom_import('hc.%s.models.GlobalOption' % (app))
+    try:
+      if global_option.objects.get(option_name='autoexclusion_status').option_value == 'disabled':
+        print '[WARNING][%s][blacklist] autoexclusion has been disabled.' % (app)
+        return 1
+    except:
+      print '[WARNING][%s][blacklist] there is a problem with the global options. autoexclusion will run by default.' % (app)
+
     # Look for the app blacklisting script and run it.
     app_script = custom_import('%s.python.scripts.server.blacklist.Blacklist' % (app))
 
