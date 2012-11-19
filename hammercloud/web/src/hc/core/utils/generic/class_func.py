422f693bfebe9cf31a1768a5859b5622ab6930c6
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-19 10:15:02 +0000

    Improved formatting and error handling.

--- hammercloud/web/src/hc/core/utils/generic/class_func.py
+++ hammercloud/web/src/hc/core/utils/generic/class_func.py
@@ -1,12 +1,23 @@
-#Method that returns an instance of the class specified
-def custom_import(name,value=None):
+"""Custom import mechanism for Python objects by name."""
 
-  try: 
-    parts = name.split('.')
-    module = ".".join(parts[:-1])
-    m = __import__( module )
-    for comp in parts[1:]:
-      m = getattr(m, comp)
-    return m
-  except:
-    return value
+# TODO(rmedrano): Remove the dependency on this kind of imports.
+
+
+def custom_import(name, value=None):
+    """Given the name of a class or function, imports it.
+
+    Arguments:
+    name -- Name of the object to import.
+    value -- Default value to return in case import fails.
+
+    Returns: the module object or the default value in case of failure.
+    """
+    try:
+        parts = name.split('.')
+        module = ".".join(parts[:-1])
+        m = __import__(module)
+        for comp in parts[1:]:
+            m = getattr(m, comp)
+        return m
+    except:
+        return value
