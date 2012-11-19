78b7180648af84f6ade5a5922d53ad8dbdb388f3
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-19 14:44:12 +0000

    Formatting and debug configuration.

--- hammercloud/web/src/hc/ssl.py
+++ hammercloud/web/src/hc/ssl.py
@@ -1,36 +1,38 @@
 from django.conf import settings
-from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, get_host
+from django.http import get_host, HttpResponsePermanentRedirect
 
-SSL = 'SSL'
+"""Django middleware to redirect to a SSL url.
 
-class SSLRedirect:
+This is used mainly for the admin panel, but could be user for all the site."""
 
-  def process_view(self, request, view_func, view_args, view_kwargs):
-    if SSL in view_kwargs:
-      secure = view_kwargs[SSL]
-      del view_kwargs[SSL]
-    else:
-      secure = False
 
-    if not secure == self._is_secure(request):
-      return self._redirect(request, secure)
+class SSLRedirect(object):
+    """Middleware to redirect in some situations to a HTTPS context."""
 
-  def _is_secure(self, request):
-    if request.is_secure():
-      return True
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        """Before answering to the request, check if SSL is needed."""
+        if settings.DEBUG:
+            return request
+        if 'SSL' in view_kwargs:
+            secure = view_kwargs['SSL']
+            del view_kwargs['SSL']
+        else:
+            secure = False
+        if not secure == self._is_secure(request):
+            return self._redirect(request, secure)
 
-    if 'HTTP_X_FORWARDED_SSL' in request.META:
-      return request.META['HTTP_X_FORWARDED_SSL'] == 'on'
+    def _is_secure(self, request):
+        """Check if we are in a HTTPS context or not."""
+        if request.is_secure():
+            return True
+        if 'HTTP_X_FORWARDED_SSL' in request.META:
+            return request.META['HTTP_X_FORWARDED_SSL'] == 'on'
+        return False
 
-    return False
-
-  def _redirect(self, request, secure):
-    protocol = secure and "https" or "http"
-    newurl = "%s://%s%s" % (protocol,get_host(request),request.get_full_path())
-    if settings.DEBUG and request.method == 'POST':
-        raise RuntimeError, \
-    """Django can't perform a SSL redirect while maintaining POST data.
-       Please structure your views so that redirects only occur during GETs."""
-
-    return HttpResponsePermanentRedirect(newurl)
-
+    def _redirect(self, request, secure):
+        """Performs the actual redirection."""
+        protocol = secure and 'https' or 'http'
+        newurl = "%s://%s%s" % (protocol, get_host(request), request.get_full_path())
+        if settings.DEBUG and request.method == 'POST':
+            raise RuntimeError('Cannot redirect to HTTPS while having POST.')
+        return HttpResponsePermanentRedirect(newurl)
