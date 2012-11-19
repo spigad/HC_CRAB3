b7623f2a3357a6077bc4b9625ef7dd3e6f10918b
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-19 15:01:22 +0000

    Formatting and cleanup of the database router.

--- hammercloud/web/src/hc/router.py
+++ hammercloud/web/src/hc/router.py
@@ -1,57 +1,37 @@
 from django.conf import settings
 
+"""Router for the database access.
+
+Allows the access to one database per app, distributing the load.
+"""
+
+PLUGINS = [app.split('.')[1] for app in settings.INSTALLED_APPS
+           if app.startswith('hc') and app != 'core']
+
+
 class PrimaryRouter(object):
+    """Router for the database access."""
 
-  def db_for_read(self, model, **hints):
+    def db_for_read(self, model, **hints):
+        """Find database for read."""
+        if model._meta.app_label in PLUGINS:
+            return model._meta.app_label
+        return None
 
-    '''
-    If the label belongs to a plugin, it reads from the pluign specific DB
-    Otherwise, it reads from the defautl DB
-    '''
+    def db_for_write(self, model, **hints):
+        """Find database for write."""
+        if model._meta.app_label in PLUGINS:
+            return model._meta.app_label
+        return None
 
-#    PLUGINS = ['atlas','cms','lhcb']
-    PLUGINS = [ app.split('.')[1] for app in settings.INSTALLED_APPS if app.startswith('hc') and not 'core' in app]
+    def allow_relation(self, obj1, obj2, **hints):
+        """The router does not forbid relations."""
+        return None
 
-    if model._meta.app_label in PLUGINS:
-      return model._meta.app_label
-
-    return None
-
-  def db_for_write(self, model, **hints):
-
-    '''
-    If the label belongs to a plugin, it reads from the pluign specific DB
-    Otherwise, it reads from the defautl DB
-    '''
-
-#    PLUGINS = ['atlas','cms','lhcb']
-    PLUGINS = [ app.split('.')[1] for app in settings.INSTALLED_APPS if app.startswith('hc') and not 'core' in app]
-
-    if model._meta.app_label in PLUGINS:
-      return model._meta.app_label
-
-    return None
-
-  def allow_relation(self, obj1, obj2, **hints):
-
-    '''
-    Literally: no opinion about objects relations and foreign keys.
-    '''
-   
-    return None
-
-  def allow_syncdb(self, db, model):
-
-    '''
-    Sync every app with the right database
-    '''
-
-#    PLUGINS = ['atlas','cms','lhcb']
-    PLUGINS = [ app.split('.')[1] for app in settings.INSTALLED_APPS if app.startswith('hc') and not 'core' in app]
-
-    if db in PLUGINS:
-      return model._meta.app_label == db
-    elif model._meta.app_label in PLUGINS:
-      return False
-    return None
-
+    def allow_syncdb(self, db, model):
+        """Sync each model to its database."""
+        if db in PLUGINS:
+            return model._meta.app_label == db
+        elif model._meta.app_label in PLUGINS:
+            return False
+        return None
