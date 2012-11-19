a9fe0a826c76c2456e29c53f9d6a00e83cd51b3b
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-19 14:22:45 +0000

    Formatting and general cleanup.

--- hammercloud/web/src/hc/urls.py
+++ hammercloud/web/src/hc/urls.py
@@ -1,24 +1,16 @@
-from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # If some HammerCloud application urls are needed, add them here.
+    # (r'^app/atlas', include('hc.atlas.urls'))
 
-  #If some application urls are needed to be 
-  #added, add them here.
-  #(r'^app/atlas', include('hc.atlas.urls'))
+    (r'', include('hc.core.urls')),
+    (r'', include('hc.core.base.rss.urls')),
+    (r'', include('hc.core.base.xhr.urls')),
+    (r'', include('hc.core.base.xmlrpc.urls')),
 
-  (r'', include('hc.core.urls')),
-  (r'', include('hc.core.base.rss.urls')),
-  (r'', include('hc.core.base.xhr.urls')),
-  (r'', include('hc.core.base.xmlrpc.urls')),
-
-  url(r'^admin/', include(admin.site.urls) , kwargs={'SSL':True}),
-  
-  (r'^admin/varnish/', include('varnishapp.urls')),
-
-#  (r'^accounts/login/$',  'django.contrib.auth.views.login', {'template_name': 'core/app/login.html'}),
-
+    url(r'^admin/', include(admin.site.urls)),
 )
