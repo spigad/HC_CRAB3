200c02cdc8818f443cd9e1f7e2c84a8401e44cdf
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-19 10:18:54 +0000

    Improve formmating and make output less verbose.

--- hammercloud/scripts/config/config-shell.sh
+++ hammercloud/scripts/config/config-shell.sh
@@ -1,30 +1,26 @@
 #!/bin/sh
 
-#ARGUMENTS: <app>
+# Wrapper to start up a Django shell with the HC environment set.
+# ARGUMENTS: <app>
 
-echo '_  HC Django Shell'
-echo ''
+echo '# HammerCloud Django Shell'
 
 if [ -z $1 ]
 then
-    echo '  ERROR! Please, set application tag.'
-    echo '  Use default = demo'
-    echo ''
+    echo '  WARNING: using default app demo'
     export HC_SHELL_APP=demo
 else
     export HC_SHELL_APP=$1
 fi
 
-#Get HCDIR from current installation.
+# Get HCDIR from current installation.
+# TODO(rmedrano): This makes mandatory to run this script with the full path.
 HCDIR=`which $0|sed 's/\/scripts/ /g'|awk '{print $1}'`
 
+# Do the config_main, for PATH and PYTHONPATH.
 source $HCDIR/scripts/config/config-main.sh $HC_SHELL_APP
 
 cd $HCDIR/web/src/hc
 
+# debugsqlshell needs django-toolbar installed.
 python manage.py debugsqlshell
-
-echo ''
-echo '_ End  HC  Django Shell.'
-echo ''
-
