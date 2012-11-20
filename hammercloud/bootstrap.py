cec7eb2d7f3157b3c6962e8a163c55f3140d4e26
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-20 10:12:29 +0000

    Add a script to install dependencies cleanly through a virtualenv.

---
+++ hammercloud/bootstrap.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""Bootstrap script to set up the Python environment
+
+Should be called after the virtualenv is activated in order to install all the
+Python modules and dependencies in the local venv. This work has been greatly
+inspired by the following Colin Copeland's article: http://goo.gl/KIMsq
+"""
+
+import os
+import subprocess
+import sys
+
+
+def main():
+    if 'VIRTUAL_ENV' not in os.environ:
+        # TODO(rmedrano): One could enable the virtualenv programatically.
+        print >> sys.stderr, '$VIRTUAL_ENV not found, please enable one.'
+        return -1
+    base_path = os.path.dirname(__file__)
+    return subprocess.call(['pip', 'install', '--requirement',
+                            os.path.join(base_path, 'requirements/apps.txt')])
+
+
+if __name__ == '__main__':
+    sys.exit(main())
