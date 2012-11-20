947682e33c9c189132cbff8c4cc1b82272e98053
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-20 14:16:48 +0000

    Add the global settings file according to the Django 1.4 guidelines.

---
+++ hammercloud/web/src/hc/settings.py
@@ -0,0 +1,173 @@
+# Django settings for hc project.
+
+import os
+
+# Points to the installation path of HammerCloud web.
+HC_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Admins and managers lists has been moved to local_settings.py
+
+# Database configuration has been moved to local_settings.py
+
+DATABASE_ROUTERS = ['hc.router.PrimaryRouter']
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Zurich'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# The login URL. Supposes that HammerCloud is installed under the /hc URL.
+LOGIN_URL = '/hc/accounts/login'
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+
+# European date formats despite the use of internationalization.
+FORMAT_MODULE_PATH = 'hc.core.formats'
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(HC_DIR, 'uploads')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = 'http://hammercloud.cern.ch/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(HC_DIR, 'static')
+
+# URL prefix for static files.
+# Examples: "http://foo.com/media/", "/media/".
+STATIC_URL = 'http://hammercloud.cern.ch/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    # TODO(rmedrano): switch to fully static naming (this is legacy from 1.2).
+    os.path.join(HC_DIR, 'media'),
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# The secret key has been moved to local_settings.py
+
+# Disable the framing of this page.
+X_FRAME_OPTIONS = 'DENY'
+
+# Activate the use of ETags to allow caching.
+USE_ETAGS = True
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'hc.ssl.SSLRedirect',
+)
+
+ROOT_URLCONF = 'hc.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'hc.wsgi.application'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates"
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(HC_DIR, 'templates'),
+)
+
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.humanize',
+    'hc.core',
+    'hc.atlas',
+    'hc.cms',
+    'hc.lhcb',
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+# Import settings from the local_settings.py module
+try:
+    from hc.local_settings import *
+    INSTALLED_APPS += EXTRA_APPS
+    if DEBUG:
+        INSTALLED_APPS += DEBUG_APPS
+        MIDDLEWARE_CLASSES += DEBUG_MIDDLEWARE
+except ImportError:
+    import logging
+    logging.warning('hc.local_settings could not be imported.')
