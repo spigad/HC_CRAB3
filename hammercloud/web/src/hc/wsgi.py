85f14f48d8f6e5a24b9438013a2aa471380fc921
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-20 14:37:11 +0000

    Enable the file monitor for all the files in the web tree.

--- hammercloud/web/src/hc/wsgi.py
+++ hammercloud/web/src/hc/wsgi.py
@@ -1,9 +1,19 @@
+import hc.monitor
 import os
+import re
 import resource
+
+SOURCES_FILTER = r'.*\\.py\\Z(?ms)|.*\\.html\\Z(?ms)'
 
 # Set resource limits to prevent machine crashes and debug.
 resource.setrlimit(resource.RLIMIT_CPU, (10, 60))  # Seconds
 resource.setrlimit(resource.RLIMIT_AS, (1073741824, 2147483648))  # Bytes
+
+# Set up the monitoring for file changes.
+for dirname, _, files in os.walk(os.path.dirname(__file__)):
+    for name in  [f for f in files if re.match(SOURCES_FILTER, f)]:
+        hc.monitor.track(os.path.join(dirname, name))
+hc.monitor.start(interval=1.0)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hc.settings')
 
