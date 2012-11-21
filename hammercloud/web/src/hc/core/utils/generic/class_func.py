697448c2e4832382c6541daf1f30c4d82464b484
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-21 16:52:25 +0000

    Formatting and error handling improvements.

--- hammercloud/web/src/hc/core/utils/generic/class_func.py
+++ hammercloud/web/src/hc/core/utils/generic/class_func.py
@@ -3,21 +3,20 @@
 # TODO(rmedrano): Remove the dependency on this kind of imports.
 
 
-def custom_import(name, value=None):
-    """Given the name of a class or function, imports it.
+def custom_import(name):
+    """Given the full name of a class or function, imports it in depth.
 
     Arguments:
-    name -- Name of the object to import.
-    value -- Default value to return in case import fails.
+    name -- Name of the object to import, must be complete.
 
     Returns: the module object or the default value in case of failure.
     """
     try:
         parts = name.split('.')
-        module = ".".join(parts[:-1])
+        module = '.'.join(parts[:-1])
         m = __import__(module)
         for comp in parts[1:]:
             m = getattr(m, comp)
         return m
-    except:
-        return value
+    except (ValueError, AttributeError):
+        raise ImportError
