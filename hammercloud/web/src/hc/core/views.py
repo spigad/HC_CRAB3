0ae8ef03f8af274f65b7ff32777602433bb70c78
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-21 14:29:53 +0000

    Formatting improvements and move to the site-defined caching.

--- hammercloud/web/src/hc/core/views.py
+++ hammercloud/web/src/hc/core/views.py
@@ -1,22 +1,23 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.template import loader, RequestContext
-from django.conf import settings
-from django.views.decorators.cache import cache_page
 
-@cache_page(60 * 60 * 24)
+"""Main module for the common views.
+
+This module only renders the portal view, the rest are in the base package.
+"""
+
+
 def portal(request):
+    """View handler for the home page, shows the list of HC apps."""
+    apps = []
 
-  apps = []
+    for app in settings.INSTALLED_APPS:
+        if app.startswith('hc'):
+            app = app.split('.')[1]
+            if app != 'core':
+                apps.append(app)
 
-  for app in settings.INSTALLED_APPS:
-    if app.startswith('hc'):
-      app = app.split('.')[1]
-      if app != 'core':
-        apps.append(app)    
-    
-  t = loader.get_template('core/portal/index.html')
-  c = RequestContext(request,
-                     {'apps': apps},
-                     )
-  return HttpResponse(t.render(c))
-
+    t = loader.get_template('core/portal/index.html')
+    c = RequestContext(request, {'apps': apps})
+    return HttpResponse(t.render(c))
