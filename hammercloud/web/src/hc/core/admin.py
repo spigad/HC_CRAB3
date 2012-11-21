0ba114c60af3549fb8e63f01225b7a6f88aeec80
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-21 16:21:22 +0000

    Format for the admin model of the plugins.

--- hammercloud/web/src/hc/core/admin.py
+++ hammercloud/web/src/hc/core/admin.py
@@ -1,20 +1,26 @@
-from hc.core.models import Plugin,HCFeed
 from django.contrib import admin
-
+from hc.core.base.admin import ConfiguredAdmin
 from hc.core.base.rss.admin import HCFeedAdminBase
+from hc.core.models import Plugin, HCFeed
 
-class PluginAdmin(admin.ModelAdmin):
+"""Admin configuration for the"""
 
-  list_display = ('name',)
-  fieldsets = [
-    ('Cloud', {'fields': ['name']})
-  ]
-#  list_filter = ['name']
-#  search_fields = ['name']
+
+class PluginAdmin(ConfiguredAdmin):
+    """ModelAdmin for the plugins."""
+    readonly_fields = ('mtime',)
+    list_display = ('name', 'mtime')
+    date_hierarchy = 'mtime'
+    fieldsets = [
+        ('Plugin', {'fields': ['name']})
+        ]
+    list_filter = ['name']
+    search_fields = ['name']
+
 
 class HCFeedAdmin(HCFeedAdminBase):
-  pass
+    pass
 
 
-admin.site.register(HCFeed,HCFeedAdmin)
-admin.site.register(Plugin,PluginAdmin)
+admin.site.register(HCFeed, HCFeedAdmin)
+admin.site.register(Plugin, PluginAdmin)
