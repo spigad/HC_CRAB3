00f765069d36f315dff22ad658773e2887c404d4
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-21 15:08:39 +0000

    Formatting and pylint warnings.

--- hammercloud/web/src/hc/core/base/views/control.py
+++ hammercloud/web/src/hc/core/base/views/control.py
@@ -1,16 +1,17 @@
 from django.contrib.auth import logout as logout_user
-from django.http import Http404,HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 
+"""View handlers for the login/logout."""
 
-#######################################################
-## LOGIN VIEWS
-#######################################################
 
 def logout(request):
-  logout_user(request)
-  next = request.GET.get('next', None)
-  if next:
-    return HttpResponseRedirect(next)
-  else:
-    raise Http404
+    """Logs out the user and redirects to the following URL.
 
+    The URL must have a GET parameter to redirect to.
+    """
+    logout_user(request)
+    following = request.GET.get('next', None)
+    if following:
+        return HttpResponseRedirect(following)
+    else:
+        raise Http404
