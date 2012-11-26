0d4f262fce24fc402055b89d23247729f45bde87
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-26 10:49:18 +0000

    Improve formatting, documentation and convert to a new style class.

--- hammercloud/web/src/hc/core/utils/generic/dic_config.py
+++ hammercloud/web/src/hc/core/utils/generic/dic_config.py
@@ -1,7 +1,11 @@
-class DicConfig:
-
-  def __init__(self,dic):
-    for k,v in dic.items():
-      setattr(self,k,v)
+"""Offers a dictionary-like class to store configurations."""
 
 
+class DicConfig(object):
+    """Class to store config values as attributes."""
+
+    def __init__(self, dic):
+        """Copies the values from the dict to attributes of this class."""
+        super(DicConfig, self).__init__()
+        for k, v in dic.iteritems():
+            setattr(self, k, v)
