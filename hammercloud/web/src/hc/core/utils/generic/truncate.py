06137e6b26958dcd7fead8489f4e1178db813172
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-26 10:56:24 +0000

    Formatting and documentation improvements. Bug solved on the .... (4x .)

--- hammercloud/web/src/hc/core/utils/generic/truncate.py
+++ hammercloud/web/src/hc/core/utils/generic/truncate.py
@@ -1,18 +1,25 @@
-def truncateList(list,top):
-    '''
+"""Implements functions to truncate list of strings (such as site names)."""
+
+
+def truncateList(lst, top):
+    """"Filter for the use in templates
+
     Filter for the Django template system date given a list of object
     and a number n return the first n elements joined by ', '.
     If the list is longer than n it appends in the end ', x more...'
     where x is the number of left elements.
-    '''
-
-    string = ', '.join(map(str, list[:top]))
-    if len(list) > top:
-      string += ', %d more...'%(len(list)-top)
+    """
+    string = ', '.join(map(str, lst[:top]))
+    if len(lst) > top:
+        string += ', %d more...' % (len(lst) - top)
     return string
 
-def truncateString(string,max):
 
-    if len(string) > max:
-      string = str(string[:max])+'....'
+def truncateString(string, max_len):
+    """Filter for the use in templates.
+
+    This is a version which trimms out the right part of the string.
+    """
+    if len(string) > max_len:
+        string = str(string[:max_len]) + '...'
     return string
