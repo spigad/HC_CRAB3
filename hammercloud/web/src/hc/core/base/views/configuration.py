cd160253fef0c784c7edc3fe54f0771d73cbfb48
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2012-11-26 12:48:15 +0000

    Improved formatting and add metrics mappings.

--- hammercloud/web/src/hc/core/base/views/configuration.py
+++ hammercloud/web/src/hc/core/base/views/configuration.py
@@ -1,12 +1,33 @@
-PARTICULARITIES = {'atlas':{'cloud':'Cloud',
-                            'base_template': 'atlas/base.html',
-                            'sls': 'HC.ATLAS',
-                            'monitoring_url': 'http://panda.cern.ch/server/pandamon/query?job='},
-                   'cms'  :{'cloud':'Region',
-                            'base_template': 'cms/base.html',
-                            'sls': 'HC.CMS',
-                            'monitoring_url': 'http://dashb-cms-job.cern.ch/dashboard/request.py/detailView?schedulerJobId='},
-                   'lhcb' :{'cloud':'T1',
+"""This dictionary provides configurations for the apps."""
+
+# TODO(rmedrano): move this to a more common place, like local_settings.py
+
+
+PARTICULARITIES = {'atlas': {'cloud': 'Cloud',
+                             'base_template': 'atlas/base.html',
+                             'sls': 'HC.ATLAS',
+                             'monitoring_url': 'http://panda.cern.ch/server/pandamon/query?job=',
+                             'metrics': {'queue': 'timetorun',
+                                         'running': 'pandatime3',
+                                         'stagein': 'pandatime2',
+                                         'stageout': 'pandatime4'},
+                             },
+                   'cms': {'cloud': 'Region',
+                           'base_template': 'cms/base.html',
+                           'sls': 'HC.CMS',
+                           'monitoring_url': 'http://dashb-cms-job.cern.ch/dashboard/request.py/detailView?schedulerJobId=',
+                           'metrics': {'queue': None,
+                                       'running': None,
+                                       'stagein': None,
+                                       'stageout': None},
+                           },
+                   'lhcb': {'cloud': 'T1',
                             'base_template': 'core/app/base.html',
                             'sls': 'HC.LHCb',
-                            'monitoring_url': None}}
+                            'monitoring_url': None,
+                            'metrics': {'queue': None,
+                                        'running': None,
+                                        'stagein': None,
+                                        'stageout': None},
+                            }
+                   }
