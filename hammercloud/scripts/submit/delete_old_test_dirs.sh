ded871c946934d533be39341b17897395debb26f
Ganga Robot <gangar.bt@cern.ch>
2013-01-10 16:05:39 +0000

    Update Django version in the clean script.

--- hammercloud/scripts/submit/delete_old_test_dirs.sh
+++ hammercloud/scripts/submit/delete_old_test_dirs.sh
@@ -5,7 +5,7 @@ export HCDIR=/data/hc
 export PATH=$HCDIR/external/bin:/afs/cern.ch/sw/lcg/external/Python/2.5.4p2/x86_64-slc5-gcc43-opt/bin:$PATH
 
 # Set the Python path.
-export PYTHONPATH=$HCDIR/apps:$HCDIR/external/django/Django-1.2.3:$HCDIR/external/lib/python2.5/site-packages:$HCDIR/web/src:/afs/cern.ch/sw/lcg/external/mysql_python/1.2.2-mysql5.0.18-python2.5/x86_64-slc5-gcc43-opt/lib/python2.5/site-packages:$PYTHONPATH:/usr/lib/python2.5/site-packages
+export PYTHONPATH=$HCDIR/apps:$HCDIR/external/django/Django-1.4.1:$HCDIR/external/lib/python2.5/site-packages:$HCDIR/web/src:/afs/cern.ch/sw/lcg/external/mysql_python/1.2.2-mysql5.0.18-python2.5/x86_64-slc5-gcc43-opt/lib/python2.5/site-packages:$PYTHONPATH:/usr/lib/python2.5/site-packages
 
 export DJANGO_SETTINGS_MODULE='hc.settings'
 
