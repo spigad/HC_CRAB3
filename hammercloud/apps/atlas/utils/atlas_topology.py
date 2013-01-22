0943df667fc89103e719d461b03f2f940bc5c57e
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-01-22 10:17:16 +0000

    Big update to the ATLAS topology mapper.

--- hammercloud/apps/atlas/utils/atlas_topology.py
+++ hammercloud/apps/atlas/utils/atlas_topology.py
@@ -2,11 +2,13 @@
 
 """Maps PanDA queue names to CE hostnames easily
 
-This module loads the topology from ATLAS services and allows to get the list
-of CE hostnames and meta information for a PanDA queue.
+This module loads the topology from ATLAS services (namely AGIS) and allows to
+get the list of CE hostnames and meta information for a PanDA queue.
 """
 
+import datetime
 import getopt
+import logging
 import sys
 import unittest
 import urllib
@@ -17,21 +19,26 @@ except ImportError:
     import json
 
 
-__author__ = 'Ramon Medrano Llamas'
-__email__ = 'ramon.medrano.llamas@cern.ch'
-__credits__ = 'Alessandro Di Girolamo'
-__version__ = '0.1'
+__author__ = 'Ramon Medrano Llamas <ramon.medrano.llamas@cern.ch>'
+__credits__ = 'Alessandro Di Girolamo <alessandro.di.girolamo@cern.ch>'
+__version__ = '0.2'
 
-ATLAS_SITES_URL = 'http://adc-ssb.cern.ch/SITE_EXCLUSION/ATLAS_sites.json'
-CE_MAPPINGS_URL = 'http://grid-monitoring.cern.ch/mywlcg/sam-pi/services_defined_in_groups?vo_name=atlas&output=json&group_type=ATLAS_Site'
+AGIS_SOURCE = 'http://atlas-agis-api.cern.ch/request/pandaqueue/query/list/?json&preset=schedconf.all'
+REBUS_SOURCE = 'http://gstat-wlcg.cern.ch/apps/capacities/sites/ALL/' + datetime.date.today().strftime('%Y') + '/' + datetime.date.today().strftime('%m') + '/json'
 
 
-class ATLASTopologyException(Exception):
+class ATLASTopologyError(Exception):
     """Errors related with this module will use this type."""
 
     def __init__(self, msg):
-        super(ATLASTopologyException, self).__init__()
+        super(ATLASTopologyError, self).__init__()
         self.msg = msg
+
+    def __unicode__(self):
+        return self.msg
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
 
 
 class ATLASTopology(object):
@@ -43,80 +50,80 @@ class ATLASTopology(object):
 
     def __init__(self):
         super(ATLASTopology, self).__init__()
-        self.__load_atlas_sites__()
-        self.__load_ce_mappings__()
+        setattr_from_remote_json(self, '__schedconfig__', AGIS_SOURCE)
+        setattr_from_remote_json(self, '__rebus__', REBUS_SOURCE)
 
-    def get_ces_for_panda_queue(self, panda_queue, flavour=None):
-        """Returns a list of CEs for the given PanDA queue."""
-        return self.get_ces_for_site(self.get_site_of_panda_queue(panda_queue),
-                                     flavour)
-
-    def get_ces_for_site(self, site, flavour=None):
+    def get_ces_from_siteid(self, panda_siteid):
         """Returns a list of CEs for the given ATLAS site."""
-        ces = filter(lambda x: x['type'] == 'CE',
-                     self.get_services_for_site(site))
-        if flavour is not None:
-            ces = filter(lambda x: x['flavour'] == flavour, ces)
-        return map(lambda x: x['hostname'], ces)
+        return map(lambda q: q['ce_endpoint'].split(':')[0],
+                   filter(lambda q: q['ce_state'] == 'ACTIVE',
+                          self._get_field_from_siteid(panda_siteid, 'queues')))
 
-    def get_site_of_panda_queue(self, panda_queue):
+    def get_total_hepspec_from_siteid(self, panda_siteid):
+        """Returns the total HEPSPEC06 count declared in REBUS data."""
+        return self._get_rebus_field_from_siteid(panda_siteid, 'HEPSPEC06')
+
+    def get_hepspec_from_siteid(self, panda_siteid):
+        """Returns the HEPSPEC/jobslot for this site."""
+        return (float(self.get_total_hepspec_from_siteid(panda_siteid)) /
+                float(self.get_jobslots_from_siteid(panda_siteid)))
+
+    def get_jobslots_from_siteid(self, panda_siteid):
+        """Returns the total jobslots count declared in REBUS data."""
+        return self._get_rebus_field_from_siteid(panda_siteid, 'LogicalCPUs')
+
+    def get_site_from_siteid(self, panda_siteid):
         """Returns the site name of a PanDA queue."""
-        # This can be done with nested map() / filter() but is more readable
-        # like this (and slower, if perfomance is not enough, should be changed
-        # to the native map() / filter()).
-        for (site_name, site_data) in self.__atlas_sites__.iteritems():
-            for (_, resources) in site_data['processing_resources'].iteritems():
-                for queue_type in ('analysis', 'production', 'special'):
-                    if panda_queue in resources[queue_type].keys():
-                        return site_name
-        return None
+        return self._get_field_from_siteid(panda_siteid, 'atlas_site')
 
-    def get_site_id_of_panda_queue(self, panda_queue, _site_type=None):
-        """Returns the SiteID name of a PanDA queue."""
-        # This can be done with nested map() / filter() but is more readable
-        # like this (and slower, if perfomance is not enough, should be changed
-        # to the native map() / filter()).
-        for (_, site_data) in self.__atlas_sites__.iteritems():
-            for (site_id, resources) in site_data['processing_resources'].iteritems():
-                for queue_type in ('analysis', 'production', 'special'):
-                    if panda_queue in resources[queue_type].keys():
-                        return site_id
-        return None
+    def _get_field_from_siteid(self, panda_siteid, field):
+        """Returns one of the multiple fields from AGIS for that siteid."""
+        return self._get_all_from_siteid(panda_siteid)[field]
 
-    def get_services_for_site(self, site):
-        """Returns a list of services for a site, including CEs, SRMs..."""
+    def _get_all_from_siteid(self, panda_siteid):
+        """Returns all the information for a given PanDA siteid.
+
+        panda_siteid -- (str) the siteid of the site to lookup.
+
+        Returns:
+          A dict with all the AGIS information. Raises an ATLASTopologyError if
+          the siteid is not found.
+        """
         try:
-            return filter(lambda x: x['name'] == site,
-                          filter(lambda x: x['name'] == 'ATLAS_Site',
-                                 filter(lambda x: x['name'] == 'atlas',
-                                        self.__ce_mappings__)[0]['group_types'])
-                          [0]['groups'])[0]['services']
+            return filter(lambda s: s['panda_resource'] == panda_siteid,
+                          self.__schedconfig__.itervalues())[0]
         except IndexError:
-            return []
+            raise ATLASTopologyError(('The PanDA siteid "%s" does not exist' %
+                                      panda_siteid))
 
-    def __load_atlas_sites__(self):
-        """Loads the ATLAS site topology from SSB and parses it."""
-        setattr_from_remote_json(self, '__atlas_sites__', ATLAS_SITES_URL)
-
-    def __load_ce_mappings__(self):
-        """Loads the ATLAS CE mappings from SAM and parses it."""
-        setattr_from_remote_json(self, '__ce_mappings__', CE_MAPPINGS_URL)
+    def _get_rebus_field_from_siteid(self, panda_siteid, field):
+        """Returns a particular field from REBUS."""
+        try:
+            return filter(lambda s: (s['Site'] ==
+                                     self._get_field_from_siteid(panda_siteid,
+                                                                 'rc_site')),
+                          self.__rebus__)[0][field]
+        except IndexError:
+            raise ATLASTopologyError(('No information in REBUS for "%s"' %
+                                      panda_siteid))
 
 
 def setattr_from_remote_json(obj, attr, url):
     """Loads a remote JSON file and sets an attrbute to an object from it."""
     try:
+        logging.debug('Downloading JSON source at %s', url)
         setattr(obj, attr, json.load(urllib.urlopen(url)))
-    except IOError, (_, msg):
-        raise ATLASTopologyException(msg)
-    except ValueError, ve:
-        raise ATLASTopologyException(str(ve))
+    except IOError as (_, msg):  # pylint: disable=E211
+        logging.exception('Could not parse the source at %s', url)
+        raise ATLASTopologyError(msg)
+    except ValueError as ve:
+        logging.exception('Could not parse the source at %s', url)
+        raise ATLASTopologyError(str(ve))
 
 
 class TestATLASToplogy(unittest.TestCase):
     """Class with the unit tests for this module."""
     # TODO(rmedrano): Add tests to this.
-    pass
 
 
 def main(argv=None):
@@ -132,7 +139,7 @@ def main(argv=None):
         try:
             opts, args = getopt.getopt(argv[1:], 'h', ['help'])
         except getopt.error, msg:
-            raise ATLASTopologyException(msg)
+            raise ATLASTopologyError(msg)
         # Process options.
         for (opt, _) in opts:
             if opt in ('-h', '--help'):
@@ -140,10 +147,10 @@ def main(argv=None):
                 return 0
         # Process arguments.
         if len(args) > 0:
-            raise ATLASTopologyException("This program doesn't allow argument")
+            raise ATLASTopologyError("This program doesn't allow arguments")
         # Launch unit tests for this module here.
         unittest.main()
-    except ATLASTopologyException, err:
+    except ATLASTopologyError, err:
         print >> sys.stderr, err.msg
         print >> sys.stderr, 'for help use --help'
         return -1
