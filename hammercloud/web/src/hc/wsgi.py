fd880469f2ea6b31ce89570ea8725b8a0e97b087
Ganga Robot <gangar.bt@cern.ch>
2013-02-11 16:24:32 +0000

    Remove limits

--- hammercloud/web/src/hc/wsgi.py
+++ hammercloud/web/src/hc/wsgi.py
@@ -1,13 +1,10 @@
 import hc.monitor
+import logging
 import os
 import re
 import resource
 
 SOURCES_FILTER = r'.*\\.py\\Z(?ms)|.*\\.html\\Z(?ms)'
-
-# Set resource limits to prevent machine crashes and debug.
-resource.setrlimit(resource.RLIMIT_CPU, (10, 60))  # Seconds
-resource.setrlimit(resource.RLIMIT_AS, (1073741824, 2147483648))  # Bytes
 
 # Setup settings.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hc.settings')
