d210a55cebadc3e83e6094c651359d2b793b1c52
Ganga Robot <gangar.bt@cern.ch>
2013-02-11 14:06:08 +0000

    Remove crap from the shell launcher

--- hammercloud/scripts/config/config-shell.sh
+++ hammercloud/scripts/config/config-shell.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 
 # Wrapper to start up a Django shell with the HC environment set.
-# ARGUMENTS: <app>
-
-echo '# HammerCloud Django Shell'
-
-if [ -z $1 ]
-then
-    echo '  WARNING: using default app demo'
-    export HC_SHELL_APP=demo
-else
-    export HC_SHELL_APP=$1
-fi
+# ARGUMENTS: <command>
 
 # Get HCDIR from current installation.
 # TODO(rmedrano): This makes mandatory to run this script with the full path.
-HCDIR=`which $0|sed 's/\/scripts/ /g'|awk '{print $1}'`
+HCDIR=`which $0|sed 's/\/scripts/ /g' | awk '{print $1}'`
 
-# Do the config_main, for PATH and PYTHONPATH.
-source $HCDIR/scripts/config/config-main.sh $HC_SHELL_APP
-
-cd $HCDIR/web/src/hc
+# Setup the virtualenv of HammerCloud
+source $HCDIR/external/bin/activate
+export PYTHONPATH=$HCDIR/python:$HCDIR/apps:$HCDIR/web/src
 
 # debugsqlshell needs django-toolbar installed.
-python manage.py shell
+python $HCDIR/web/src/hc/manage.py ${1:-shell}
