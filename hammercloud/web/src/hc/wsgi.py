869b1b9af489e2e1193572706bb9b4a5363a929a
Ganga Robot <gangar.bt@cern.ch>
2013-02-12 13:29:24 +0000

    Prevent thread spawning for monitoring on production.

--- hammercloud/web/src/hc/wsgi.py
+++ hammercloud/web/src/hc/wsgi.py
@@ -1,8 +1,5 @@
-import hc.monitor
 import logging
 import os
-import re
-import resource
 
 SOURCES_FILTER = r'.*\\.py\\Z(?ms)|.*\\.html\\Z(?ms)'
 
@@ -13,6 +10,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hc.settings')
 from django.conf import settings
 
 if settings.DEBUG:
+    import hc.monitor
+    import re
     for dirname, _, files in os.walk(os.path.dirname(__file__)):
         for name in  [f for f in files if re.match(SOURCES_FILTER, f)]:
             hc.monitor.track(os.path.join(dirname, name))
