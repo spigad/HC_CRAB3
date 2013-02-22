b103595ee6f597db615e15d15f4138b55f16c485
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-02-22 13:27:30 +0000

    Made the ATLAS submission script point to the main config.

--- hammercloud/apps/atlas/scripts/config/config-submit.sh
+++ hammercloud/apps/atlas/scripts/config/config-submit.sh
@@ -1,71 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._'
-echo '    _                                                                 _'
-echo '    _                        ATLAS Configuration                      _'
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._' 
-echo '    _'
-echo '    _'
+# Script that sets up the main environment for HammerCloud scripts for ATLAS.
+# ARGUMENTS: -m <mode> (if not set, defaults to 'default').
 
+echo 'Setting up HammerCloud ATLAS submission environment...'
 
-# Possible options are:
-#   m (mode): type of test_generate and runtest.py
-#
-
-args=`getopt -o m: -- "$@"`
-eval set -- "$args"
-
-for i
-do
-  case "$i" in
-        -m) shift;echo '    _ HC_MODE: '$1;HC_MODE=$1;shift;;
-        --) shift
-  esac
-done
-
-if [ -z $HC_MODE ]
-then
-    echo '    _ No MODE provided'
-    echo '    _ Using default HC_MODE=default'
-    HC_MODE="default"
-fi
-
-if [ "$HC_MODE" != default -a "$HC_MODE" != t3 -a "$HC_MODE" != prod -a "$HC_MODE" != experimental ]
-then
-    echo '    _ Wrong mode, '$HC_MODE
-    echo '    _ Using default HC_MODE=default'
-    HC_MODE="default"
-fi
-
-export HC_MODE=$HC_MODE
-echo '  HC_MODE='$HC_MODE
-
-if [ "$HC_MODE" == prod ]
-then
-    #Set PROXY for Production testing
-    export X509_USER_PROXY=$HCAPP/config/x509rprod
+# Source the config-main script.
+APPDIR=`which $0 | sed 's/\/scripts/ /g' | awk '{print $1}'`
+if [ -e $APPDIR/scripts/config/config-main.sh ] ; then
+    source $APPDIR/scripts/config/config-main.sh $@
 else
-    export X509_USER_PROXY=$HCAPP/config/x509rp
+    echo ' ERROR: app config-main.sh not found!'
+    exit
 fi
-echo '  X509_USER_PROXY='$X509_USER_PROXY
 
-HAMMERCLOUD_ORIGINAL_PYTHONPATH=$PYTHONPATH:$HCDIR/external/ganga/install/HEAD/python
-HAMMERCLOUD_ORIGINAL_PATH=$PATH
-
-# Handle test job generation (starttime <= now and generated==0)
-#   creates testdirs/test_N/jobs
-#source /afs/cern.ch/atlas/offline/external/GRID/ddm/DQ2Clients/setup.sh
-source /opt/dq2/profile.d/dq2_common_env.sh
-export DQ2_ENDUSER_SETUP=True
-export RUCIO_ACCOUNT=gangarbt
-export DQ2_LOCAL_SITE_ID=ROAMING
-export PYTHONPATH=/opt/dq2/lib:$PYTHONPATH
-echo '  Sourced DQ2 Client.'
-
-#Dirty hack to use our own Ganga, but with our environment
-export PYTHONPATH=$HAMMERCLOUD_ORIGINAL_PYTHONPATH:$PYTHONPATH
-export PATH=$HAMMERCLOUD_ORIGINAL_PATH:$PATH
-
-echo '    _'
-echo '    _                    End  ATLAS  Configuration                    _'
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._'
+# Custom configurations for submission may follow.
