37e665ba0fc5887baa79d756219791bdb754b310
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-02-22 13:55:48 +0000

    Removed some duplicated configuration from LHCb config-main.sh

--- hammercloud/apps/lhcb/scripts/config/config-main.sh
+++ hammercloud/apps/lhcb/scripts/config/config-main.sh
@@ -1,38 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._'
-echo '    _                                                                 _'
-echo '    _                     LHCb MAIN Configuration                     _'
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._' 
-echo '    _'
-echo '    _'
+# Script that sets up the main environment for HammerCloud scripts for LHCb.
 
-echo '    Restore the original PATH and PYTHONPATH'
+echo 'Setting up HammerCloud LHCb environment...'
 
-export PATH=$HC_ORIGINAL_PATH
-export PYTHONPATH=$HC_ORIGINAL_PYTHONPATH
-
-echo '    Source /opt/dirac/bashrc'
-
+# Source the LHCbDIRAC tools.
+echo 'Sourcing LHCbDIRAC...'
 source /opt/dirac/bashrc
-
-#Export custom site-packages
-export PATH=$PATH:$HCDIR/external/bin/
-export PYTHONPATH=$PYTHONPATH:$HCDIR/external/lib/python2.5/site-packages:$HCDIR/external/lib64/python2.5/site-packages
-
-#Export hammercloud python zone.
-export PYTHONPATH=$PYTHONPATH:$HCDIR/python
-
-#Export Django & HammerCloud.
-export PYTHONPATH=$PYTHONPATH:$HCDIR/external/django/$version:$HCDIR/web/src
-
-#Export apps zones
-export PYTHONPATH=$PYTHONPATH:$HCDIR/apps
-
-echo '  PATH='$PATH
-echo '  PYTHONPATH='$PYTHONPATH
-
-echo ''
-echo '    _ End  LHCb Main  Configuration.'
-echo ''
-
