2343d106dfe2edc72c8f16d0b8d280ce23eba9fd
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-02-22 13:52:31 +0000

    removed the demo app, which is not maintained any more.

--- hammercloud/apps/demo/python/lib/alarms/app_alarms.py
+++
@@ -1,9 +0,0 @@
-from lib.alarms.default_alarms import DefaultAlarms
-from lib.alarms.alarm import Alarm
-
-class AppAlarms(DefaultAlarms):
-
-  def __init__(self):
-    super(AppAlarms,self).__init__()
-    self.alarm3 = Alarm('3>1')
-
