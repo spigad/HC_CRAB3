02e53a0747d22f402bc27d9df29f15ef96ad4742
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-02-22 15:00:35 +0000

    Don't call config-main implicitly.

--- hammercloud/apps/atlas/scripts/config/config-submit.sh
+++ hammercloud/apps/atlas/scripts/config/config-submit.sh
@@ -5,13 +5,4 @@
 
 echo 'Setting up HammerCloud ATLAS submission environment...'
 
-# Source the config-main script.
-HCAPP=`which $0 | sed 's/\/scripts/ /g' | awk '{print $1}'`
-if [ -e $HCAPP/scripts/config/config-main.sh ] ; then
-    source $HCAPP/scripts/config/config-main.sh $*
-else
-    echo ' ERROR: app config-main.sh not found!'
-    exit
-fi
-
 # Custom configurations for submission may follow.
