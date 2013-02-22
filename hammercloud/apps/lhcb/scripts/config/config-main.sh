cd6eadcb7f791d0b2a8a9b0b5f3b8028cb5710fa
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-02-22 14:10:25 +0000

    Streamlined and unified the contents of the LHCb config scripts.

--- hammercloud/apps/lhcb/scripts/config/config-main.sh
+++ hammercloud/apps/lhcb/scripts/config/config-main.sh
@@ -1,9 +1,64 @@
 #!/bin/bash
 
 # Script that sets up the main environment for HammerCloud scripts for LHCb.
+# ARGUMENTS: -r <role> (proxy role {production, user}).
+#            -m <mode> (if not set, defaults to 'default').
 
 echo 'Setting up HammerCloud LHCb environment...'
+
+# Parse the arguments to find the mode. Save the args to avoid collaterals.
+HC_MODE='default'
+HC_ROLE='user'
+ARGS=$*
+set -- `getopt -u -o r:v:m: -- $@`
+
+while [ $# -gt 0 ] ; do
+    case $1 in
+        -r) shift; export HC_ROLE=$1; shift;;
+        -m) shift; export HC_MODE=$1; shift;;
+        *) shift;;
+    esac
+done
+
+# Restore the args (caller scripts might don't want to have them changed).
+set -- $ARGS
+
+# Validate the HC_ROLE setup.
+if [ "$HC_ROLE" != user -a "$HC_ROLE" != production ] ; then
+    echo ' ERROR: wrong role selected.'
+    exit
+fi
+echo ' HC_ROLE='$HC_ROLE
+
+# Validate the HC_MODE setup.
+if [ "$HC_MODE" != default -a "$HC_MODE" != site ] ; then
+    echo ' ERROR: wrong mode selected.'
+    exit
+fi
+echo ' HC_MODE='$HC_MODE
+
+# Setup the proxies.
+HCAPP=`which $0 | sed 's/\/scripts/ /g' | awk '{print $1}'`
+export X509_USER_PROXY=$HCAPP/config/x509up_$HC_ROLE
+echo ' X509_USER_PROXY='$X509_USER_PROXY
+
+# Save the current paths, so we can restore them after sourcing PanDA tools.
+HAMMERCLOUD_ORIGINAL_PYTHONPATH=$PYTHONPATH
+HAMMERCLOUD_ORIGINAL_PATH=$PATH
 
 # Source the LHCbDIRAC tools.
 echo 'Sourcing LHCbDIRAC...'
 source /opt/dirac/bashrc
+
+echo 'Sourcing Grid Environment from AFS...'
+source /afs/cern.ch/project/gd/LCG-share/sl4/etc/profile.d/grid-env.sh
+
+echo 'Sourcing LbLogin from AFS...'
+source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v5r1/InstallArea/scripts/LbLogin.sh
+
+echo 'SetupProject Ganga...'
+SetupProject Ganga
+
+# Restore the original paths, so we use our own Python and Ganga.
+export PYTHONPATH=$HAMMERCLOUD_ORIGINAL_PYTHONPATH:$PYTHONPATH
+export PATH=$HAMMERCLOUD_ORIGINAL_PATH:$PATH
--- hammercloud/apps/lhcb/scripts/config/config-submit.sh
+++ hammercloud/apps/lhcb/scripts/config/config-submit.sh
@@ -1,92 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._'
-echo '    _                                                                 _'
-echo '    _                        LHCb Configuration                       _'
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._' 
-echo '    _'
-echo '    _'
+# Script that sets up the main environment for HammerCloud scripts for LHCb.
+# ARGUMENTS: -r <role> (proxy role {production, user}).
+#            -m <mode> (if not set, defaults to 'default').
 
-#if [ -z $1 ]
-#then
-#    echo '    _  ERROR! Please, set CMSSW_VERSION.'
-#    echo '    _'
-#    echo '    _                      End  CMS  Configuration                    _'
-#    echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._'
-#    exit
-#fi
+echo 'Setting up HammerCloud LHCb submit environment...'
 
-# Possible options are:
-#   r (role)   : proxy role, values accepted are production,user.  
-#   m (mode): type of test_generate and runtest.py
-#
-
-args=`getopt -o r:m: -- "$@"`
-eval set -- "$args"
-
-for i
-do
-  case "$i" in
-        -r) shift;echo '    _ HC_ROLE '$1;HC_ROLE=$1;shift;;
-        -m) shift;echo '    _ HC_MODE: '$1;HC_MODE=$1;shift;;
-  esac
-done
-
-if [ -z $HC_ROLE ]
-then
-    echo '    _ No HC_ROLE provided'
-    echo '    _ Using default HC_ROLE=user'
-    HC_ROLE="user"
+# Source the config-main script.
+HCAPP=`which $0 | sed 's/\/scripts/ /g' | awk '{print $1}'`
+if [ -e $HCAPP/scripts/config/config-main.sh ] ; then
+    source $HCAPP/scripts/config/config-main.sh $*
+else
+    echo ' ERROR: app config-main.sh not found!'
+    exit
 fi
 
-if [ "$HC_ROLE" != user -a "$HC_ROLE" != production ]
-then
-    echo '    _ Wrong role, '$HC_ROLE
-    echo '    _ Using default HC_ROLE=user'
-    ROLE="user"
-fi
-
-if [ -z $HC_MODE ]
-then
-    echo '    _ No MODE provided'
-    echo '    _ Using default HC_MODE=default'
-    HC_MODE="default"
-fi
-
-if [ "$HC_MODE" != default -a "$HC_MODE" != site ]
-then
-    echo '    _ Wrong mode, '$HC_MODE
-    echo '    _ Using default HC_MODE=default'
-    HC_MODE="default"
-fi
-
-export HC_MODE=$HC_MODE
-echo '  HC_MODE='$HC_MODE
-
-export X509_USER_PROXY=$HCAPP/config/x509up_$HC_ROLE
-echo '  X509_USER_PROXY='$X509_USER_PROXY
-
-HAMMERCLOUD_ORIGINAL_PYTHONPATH=$PYTHONPATH:$HCDIR/external/ganga/install/HEAD/python
-HAMMERCLOUD_ORIGINAL_PATH=$PATH 
-
-#export HOME=$HCDIR/external/$APP
-#Done this to avoid recurrent problem with .rhosts...
-#echo '    _ HOME='$HCDIR/external/$APP
-
-echo '    _  Sourced GridEnvironment'
-source /afs/cern.ch/project/gd/LCG-share/sl4/etc/profile.d/grid-env.sh
-
-echo '    _  Sourced LbLogin'
-source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v5r1/InstallArea/scripts/LbLogin.sh
-
-echo '    _  SetupProject Ganga'
-SetupProject Ganga
-
-#Dirty hack to use our own Ganga, but with our environment
-export PYTHONPATH=$HAMMERCLOUD_ORIGINAL_PYTHONPATH:$PYTHONPATH
-export PATH=$HAMMERCLOUD_ORIGINAL_PATH:$PATH
-
-
-echo '    _'
-echo '    _                    End  LHCb  Configuration                     _'
-echo '    _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._'
+# Custom configurations for submission may follow.
