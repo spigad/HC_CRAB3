363de49ea9edf59b8c2d1ff853b1b07affc27a26
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-05-22 13:56:48 +0000

    Proper handling of arguments

--- hammercloud/scripts/config/config-shell.sh
+++ hammercloud/scripts/config/config-shell.sh
@@ -3,9 +3,12 @@
 # Wrapper to start up a Django shell with the HC environment set.
 # ARGUMENTS: <command> (a Django CLI command, by default shell)
 
+COMMAND=$1
+shift
+
 # Setup HammerCloud.
 HCDIR=`which $0 | sed 's/\/scripts/ /g' | awk '{print $1}'`
 source $HCDIR/scripts/config/config-main.sh
 
 # Launch the Django CLI.
-python $HCDIR/web/src/hc/manage.py ${1:-shell}
+python $HCDIR/web/src/hc/manage.py ${COMMAND:-shell}
