f9cb40f00af9ffe5cfcff040be973a6d7f265477
Ramón Medrano Llamas <ramon.medrano@cern.ch>
2013-05-24 13:34:41 +0000

    Cleaned and refactored the common external queue update.

--- hammercloud/python/scripts/server/external_queue_update.py
+++ hammercloud/python/scripts/server/external_queue_update.py
@@ -1,21 +1,32 @@
 from hc.core.utils.generic.class_func import custom_import
+import logging
 
-class ExternalQueueUpdate:
+"""Crontab action to monitor changes on the WMS system of the VO.
 
-  def run(self, app, dic):
+Currenty, this cron is used mainly by ATLAS, to monitor external changes of the
+queue status in order to detect external blacklists and to monitor when the
+version of the Athena nightlies are deployed.
+"""
 
-    if app == 'core':
-      print '[ERROR][%s][external_queue_update] not available at core app.' % (app)
-      return 1
+APP_SCRIPTS = (
+    '%s.python.scripts.server.external_queue_update.ExternalQueueUpdate',
+    '%s.python.scripts.server.check_nightly.CheckNightly'
+)
 
-    # Look for the app blacklisting script and run it.
-    app_script = custom_import('%s.python.scripts.server.external_queue_update.ExternalQueueUpdate' % (app))
 
-    if app_script:
-      try:
-        app_script().run()
-      except:
-        print '[ERROR][%s][external_queue_update] app external_queue_update script failed.' % (app)
-        return 0
+class ExternalQueueUpdate(object):
+    """Launch the app montoring schema."""
 
-    return 1
+    def run(self, app, dic):
+        """Launches the monitoring scripts one by one."""
+        if app != 'atlas':
+            logging.error('This cron action is only available for ATLAS')
+            return False
+
+        for script in APP_SCRIPTS:
+            logging.info('Launching app script: "%s"', script)
+            script_object = custom_import(script % app)
+            if script_object:
+                script_object().run()
+
+        return True
