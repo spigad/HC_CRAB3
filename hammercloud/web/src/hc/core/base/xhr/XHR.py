d1b95f0e3bff57d3090738194eea1006132ac0b7
Ganga Robot <ganga.rb@cern.ch>
2013-11-12 12:06:25 +0100

    Remove deprecation warning from using simplejson

--- hammercloud/web/src/hc/core/base/xhr/XHR.py
+++ hammercloud/web/src/hc/core/base/xhr/XHR.py
@@ -1,30 +1,15 @@
 from django.http import HttpResponse
 from hc.core.base.xhr.parser import XhrQueryParser
-from django.utils import simplejson
+import json
+
 
 def jsonXhr(request,app):
+  xqp = XhrQueryParser()
 
-  xqp   = XhrQueryParser()
+  data = json.dumps(xqp.parse(request,app))
+  data = data.replace('null', 'None')
+  data = data.replace('true', 'True')
+  data = data.replace('false', 'False')
 
-  data  = xqp.parse(request,app) 
+  return HttpResponse(data, 'application/javascript')
 
-  data = simplejson.dumps(data)
-  
-  data = data.replace('null','None')
-  data = data.replace('true','True')
-  data = data.replace('false','False')
-
-  return HttpResponse(data,'application/javascript')
-
-
-#Maybe implemented on a paralell Universe..
-
-#def yamlXhr(request,app):
-# xqp   = XhrQueryParser()
-# data  = xqp.parse(request,app)
-# return HttpResponse('comming soon')
-
-#def xmlXhr(request,app):
-# xqp   = XhrQueryParser()
-# data  = xqp.parse(request,app)
-# return HttpResponse('coming soon')
