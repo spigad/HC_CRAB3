c8011e0230f5a3cdd45ebec3c8e7faaee68d40ea
Ganga Robot <ganga.rb@cern.ch>
2013-11-12 11:49:34 +0100

    Improve config-shell to use apps

--- hammercloud/scripts/config/config-shell.sh
+++ hammercloud/scripts/config/config-shell.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Wrapper to start up a Django shell with the HC environment set.
-# ARGUMENTS: <command> (a Django CLI command, by default shell)
+# ARGUMENTS: <app> (the HammerCloud app name)
+#            <command> (a Django CLI command, by default shell)
 
+APP=$1
 COMMAND=$*
 
 # Setup HammerCloud.
 HCDIR=`which $0 | sed 's/\/scripts/ /g' | awk '{print $1}'`
 source $HCDIR/scripts/config/config-main.sh
 
-# Launch the Django CLI.
+# Launch the Django CLI (remove the app name).
+shift
 python $HCDIR/web/src/hc/manage.py ${COMMAND:-shell}
+
